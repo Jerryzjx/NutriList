15a2fdf1120199e5b4b703bbfcceac9c8f92575f
JerryZhang0920 <jinxuan0920@gmail.com>
2024-01-07 11:59:23 -0500

    added authentication functionality

---
+++ NutriList/Supabase/Auth/AuthManager.swift
@@ -0,0 +1,54 @@
+//
+//  AuthManager.swift
+//  NutriList
+//
+//  Created by leonard on 2024-01-07.
+//
+
+import Foundation
+import Supabase
+
+struct AppUser {
+    let uid: String
+    let email: String?
+}
+
+class AuthManager {
+    
+    static let shared = AuthManager()
+    
+    private init(){}
+    
+    let client = SupabaseClient(supabaseURL: "https://yzejsbrnewsogjycbqyj.supabase.co", supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6ZWpzYnJuZXdzb2dqeWNicXlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQzODU1NTksImV4cCI6MjAxOTk2MTU1OX0.iGRB9PJiojWWIl638ajfeTYQMNzlCjfyepcD-FDEFVM")
+    
+    func getCurrentSession() async throws -> AppUser {
+        let session = try await client.auth.session
+        return AppUser(uid: session.user.id.uuidString, email:session.user.email)
+    }
+    
+    // MARK: Registeration
+    func registerNewUserWithEmail(email:String, password: String) async throws -> AppUser {
+        let regAuthResponse = try await client.auth.signUp(email: email, password: password)
+        guard let session = regAuthResponse.session else {
+            print("no session when registering user")
+            throw NSError()
+        }
+        return AppUser(uid: session.user.id.uuidString, email:session.user.email)
+    }
+    
+    
+    // MARK: Sign In
+    func signInWithEmail(email: String, password: String) async throws -> AppUser {
+        let session = try await client.auth.signIn(email: email, password: password)
+        return AppUser(uid: session.user.id.uuidString, email:session.user.email)
+    }
+    
+    func signInWithApple(idToken: String, nonce: String) async throws -> AppUser {
+            let session = try await client.auth.signInWithIdToken(credentials: .init(provider: .apple, idToken: idToken, nonce: nonce))
+        return AppUser(uid: session.user.id.uuidString, email:session.user.email)
+    }
+    
+    func signOut() async throws {
+        try await client.auth.signOut()
+    }
+}
---
+++ NutriList/View/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  NutriList
+//
+//  Created by leonard on 2024-01-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var appUser: AppUser? = nil
+    
+    var body: some View {
+        ZStack {
+            if let appUser = appUser {
+                ToDoView(appUser: appUser)
+            } else {
+                SignInView(appUser: $appUser)
+            }
+        }
+        .onAppear {
+            Task {
+                self.appUser = try await AuthManager.shared.getCurrentSession()
+            }
+        }
+        
+       
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(appUser: nil)
+    }
+}
