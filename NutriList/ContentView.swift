dad7bcae4caaeb0872d953895115bd279f8f06d9
JerryZhang0920 <jinxuan0920@gmail.com>
2024-01-07 11:23:32 -0500

    Initial Commit

---
+++ NutriList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NutriList
+//
+//  Created by leonard on 2024-01-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NutriList/NutriListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriListApp.swift
+//  NutriList
+//
+//  Created by leonard on 2024-01-07.
+//
+
+import SwiftUI
+
+@main
+struct NutriListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
