5ad444ce92c296dff71aa013aa479665dd4e391c
JerryZhang0920 <jinxuan0920@gmail.com>
2024-01-20 20:54:54 -0500

    Update AppTextField.swift

--- NutriList/View/UIComponents/AppTextField.swift
+++ NutriList/View/UIComponents/AppTextField.swift
@@ -5,26 +5,36 @@
 //  Created by leonard on 2024-01-07.
 //
 
+//
+//  AppTextField.swift
+//  TextFields
+//
+//  Created by leonard on 2024-01-19.
+//
+
 import SwiftUI
 
 struct AppTextField: View {
     var placeHolder: String
     @Binding var text: String
-    
+
     var body: some View {
         TextField(placeHolder, text: $text)
             .padding()
-            .overlay {
+            .background {
                 RoundedRectangle(cornerRadius: 20)
                     .stroke(Color(uiColor: .secondaryLabel), lineWidth: 1)
             }
+            .accessibilityIdentifier(placeHolder)
+            .keyboardType(.emailAddress)
             .textInputAutocapitalization(.never)
             .autocorrectionDisabled()
     }
 }
 
+
 struct AppTextField_Previews: PreviewProvider {
     static var previews: some View {
-        AppTextField(placeHolder: "Email address", text: .constant(""))
+        AppTextField(placeHolder: "Email", text: .constant(""))
     }
 }
