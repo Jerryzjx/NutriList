e1fc77e18858646ae185e963252974096903db10
JerryZhang0920 <jinxuan0920@gmail.com>
2024-01-25 14:43:25 -0500

    Update RegistrationView.swift

--- NutriList/View/RegistrationView.swift
+++ NutriList/View/RegistrationView.swift
@@ -17,41 +17,61 @@ struct RegistrationView: View {
     @Binding var appUser: AppUser?
     
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
+            Spacer()
+            Spacer()
+            AppIcon()
+            Text("Sign Up")
+                .font(.largeTitle)
+                .bold()
+                .lineSpacing(0.83)
+                .foregroundColor(.white)
+                .fixedSize(horizontal: false, vertical: true)
+                .frame(width: UIScreen.main.bounds.width * 0.85, alignment: .topLeading)
+                .padding([.leading, .trailing])
+            
             VStack (spacing: 10){
                 AppTextField(placeHolder: "Enter Your Email address", text: $email)
                 AppSecureField(placeHolder: "Password at least 7 characters long", text: $password)
             }
             .padding(.horizontal, 24)
-        }
-        
-        Button {
-            Task {
-                do {
-                    let user = try await viewModel.registerNewUserWithEmail(email: email, password: password)
-                    self.appUser = appUser
-                    dismiss.callAsFunction()
-                } catch {
-                    print("issue with sign in")
-                    self.alertMessage = "Invalid email or password."
-                    self.showAlert = true
+            
+            
+            Button {
+                Task {
+                    do {
+                        let user = try await viewModel.registerNewUserWithEmail(email: email, password: password)
+                        self.appUser = appUser
+                        dismiss.callAsFunction()
+                    } catch {
+                        print("issue with sign in")
+                        UINotificationFeedbackGenerator().notificationOccurred(.error)
+                        self.alertMessage = "Invalid email or password."
+                        self.showAlert = true
+                    }
                 }
+            } label: {
+                Text("Sign up")
+                    .font(Font.system(size: 23))
+                    .fontWeight(.semibold)
+                    .padding()
+                    .foregroundColor(Color(uiColor: .white))
+                    .frame(maxWidth:.infinity)
+                    .frame(height: 55)
             }
-        } label: {
-            Text("Register")
-                .padding()
-                .foregroundColor(Color(uiColor: .systemBackground))
-                .frame(maxWidth:.infinity)
-                .frame(height: 55)
-                .background {
-                    RoundedRectangle(cornerRadius: 20, style: .continuous).foregroundColor(Color(uiColor: .label))
-                }
+            .background {
+                RoundedRectangle(cornerRadius: 15, style: .continuous).foregroundColor(Color("LightTeal"))
+            }
+            .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.1)), radius: 4, x: 0, y: 2)
+            .padding(.horizontal, 24)
+            .alert(isPresented: $showAlert) { // Alert modifier
+                Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
+            Spacer()
+            Spacer()
         }
-        .padding(.top, 12)
-        .padding(.horizontal, 24)
-        .alert(isPresented: $showAlert) { // Alert modifier
-                    Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-                }
+        .padding(10)
+        .background(Color("DarkTeal"))
     }
     
 }
